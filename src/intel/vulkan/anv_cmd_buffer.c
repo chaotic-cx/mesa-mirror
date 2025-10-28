bminor/mesa-mesa

commit e76ed91d3fb726128f96d07e479965933efcb1f1

anv: switch over to runtime pipelines
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34872>

src/intel/vulkan/anv_private.h
@@ -1224,7 +1224,6 @@ struct anv_shader {
    struct anv_state kernel;
 
    const struct brw_stage_prog_data *prog_data;
-   uint32_t prog_data_size;
 
    struct brw_compile_stats stats[3];
    uint32_t num_stats;
@@ -2185,6 +2184,11 @@ struct anv_gfx_dynamic_state {
    struct {
       uint32_t PrimitiveTopologyType;
    } vft;
+
+   /* 3DSTATE_VS */
+   struct {
+      bool     VertexCacheDisable;
+   } vs;
 
    /* 3DSTATE_VIEWPORT_STATE_POINTERS_CC */
    struct {
@@ -4422,7 +4426,7 @@ struct anv_cmd_graphics_state {
    struct anv_cmd_pipeline_state base;
 
    /* Shaders bound */
-   struct anv_shader_bin *shaders[ANV_GRAPHICS_SHADER_STAGE_COUNT];
+   struct anv_shader *shaders[ANV_GRAPHICS_SHADER_STAGE_COUNT];
 
    /* Bitfield of valid entries in the shaders array */
    VkShaderStageFlags active_stages;
@@ -4435,6 +4439,9 @@ struct anv_cmd_graphics_state {
 
    bool kill_pixel;
    bool uses_xfb;
+
+   /* Shader stage in base.shaders[] responsible for streamout */
+   mesa_shader_stage streamout_stage;
 
    /* Render pass information */
    VkRenderingFlags rendering_flags;
@@ -4530,7 +4537,7 @@ struct anv_cmd_graphics_state {
 struct anv_cmd_compute_state {
    struct anv_cmd_pipeline_state base;
 
-   struct anv_shader_bin *shader;
+   struct anv_shader *shader;
 
    bool pipeline_dirty;
 
@@ -4550,6 +4557,8 @@ struct anv_cmd_ray_tracing_state {
       struct anv_bo *bo;
       struct brw_rt_scratch_layout layout;
    } scratch;
+
+   VkDeviceSize scratch_size;
 
    uint32_t debug_marker_count;
    uint32_t num_tlas;
@@ -5022,6 +5031,12 @@ void
 anv_cmd_buffer_update_pending_query_bits(struct anv_cmd_buffer *cmd_buffer,
                                          enum anv_pipe_bits flushed_bits);
 
+void
+anv_cmd_buffer_bind_shaders(struct vk_command_buffer *cmd_buffer,
+                            uint32_t stage_count,
+                            const mesa_shader_stage *stages,
+                            struct vk_shader ** const shaders);
+
 /**
  * A allocation tied to a command buffer.
  *
@@ -5083,7 +5098,7 @@ enum anv_cmd_saved_state_flags {
 struct anv_cmd_saved_state {
    uint32_t flags;
 
-   struct anv_pipeline *pipeline;
+   struct vk_shader *shader;
    struct anv_descriptor_set *descriptor_set[MAX_SETS];
    uint8_t push_constants[MAX_PUSH_CONSTANTS_SIZE];
 };
@@ -5443,7 +5458,6 @@ struct anv_graphics_pipeline {
       memcpy(dw, &(pipeline)->batch_data[_cmd_state->offset],           \
              4 * _cmd_state->len);                                      \
    } while (0)
-
 
 struct anv_compute_pipeline {
    struct anv_pipeline                          base;
@@ -6483,6 +6497,15 @@ anv_cmd_flush_buffer_write_cp(VkCommandBuffer cmd_buffer);
 
 VkResult
 anv_cmd_buffer_ensure_rcs_companion(struct anv_cmd_buffer *cmd_buffer);
+
+void
+anv_cmd_buffer_set_rt_state(struct vk_command_buffer *vk_cmd_buffer,
+                            VkDeviceSize scratch_size,
+                            uint32_t ray_queries);
+
+void
+anv_cmd_buffer_set_stack_size(struct vk_command_buffer *vk_cmd_buffer,
+                              VkDeviceSize stack_size);
 
 bool
 anv_can_hiz_clear_image(struct anv_cmd_buffer *cmd_buffer,

src/intel/vulkan/anv_shader_compile.c
@@ -1886,5 +1886,8 @@ struct vk_device_shader_ops anv_device_shader_ops = {
    .deserialize                    = anv_shader_deserialize,
    .write_rt_shader_group          = anv_write_rt_shader_group,
    .write_rt_shader_group_replay_handle = anv_write_rt_shader_group_replay_handle,
+   .cmd_bind_shaders               = anv_cmd_buffer_bind_shaders,
    .cmd_set_dynamic_graphics_state = vk_cmd_set_dynamic_graphics_state,
+   .cmd_set_rt_state               = anv_cmd_buffer_set_rt_state,
+   .cmd_set_stack_size             = anv_cmd_buffer_set_stack_size,
 };

src/intel/vulkan/anv_astc_emu.c
@@ -5,6 +5,8 @@
 
 #include "anv_private.h"
 
+#include "vk_common_entrypoints.h"
+
 #include "compiler/nir/nir_builder.h"
 
 static void
@@ -293,8 +295,9 @@ astc_emu_flush_denorm_slice(struct anv_cmd_buffer *cmd_buffer,
                                      set_writes);
    VkDescriptorSet set = anv_descriptor_set_to_handle(&push_set.set);
 
-   anv_CmdBindPipeline(cmd_buffer_, VK_PIPELINE_BIND_POINT_COMPUTE,
-                       astc_emu->pipeline);
+   vk_common_CmdBindPipeline(cmd_buffer_,
+                             VK_PIPELINE_BIND_POINT_COMPUTE,
+                             astc_emu->pipeline);
 
    VkPushConstantsInfoKHR push_info = {
       .sType = VK_STRUCTURE_TYPE_PUSH_CONSTANTS_INFO_KHR,
@@ -351,7 +354,9 @@ astc_emu_decompress_slice(struct anv_cmd_buffer *cmd_buffer,
       return;
    }
 
-   anv_CmdBindPipeline(cmd_buffer_, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline);
+   vk_common_CmdBindPipeline(cmd_buffer_,
+                             VK_PIPELINE_BIND_POINT_COMPUTE,
+                             pipeline);
 
    struct vk_texcompress_astc_write_descriptor_set writes;
    vk_texcompress_astc_fill_write_descriptor_sets(astc_emu->texcompress,

src/intel/vulkan/anv_device.c
@@ -31,6 +31,7 @@
 
 #include "anv_private.h"
 #include "anv_measure.h"
+#include "anv_shader.h"
 #include "anv_slab_bo.h"
 #include "util/u_debug.h"
 #include "util/os_file.h"
@@ -379,6 +380,8 @@ VkResult anv_CreateDevice(
                            &dispatch_table, pCreateInfo, pAllocator);
    if (result != VK_SUCCESS)
       goto fail_alloc;
+
+   device->vk.shader_ops = &anv_device_shader_ops;
 
    if (INTEL_DEBUG(DEBUG_BATCH) || INTEL_DEBUG(DEBUG_BATCH_STATS)) {
       for (unsigned i = 0; i < physical_device->queue.family_count; i++) {

src/intel/vulkan/anv_genX.h
@@ -223,7 +223,7 @@ uint32_t
 genX(cmd_buffer_flush_descriptor_sets)(struct anv_cmd_buffer *cmd_buffer,
                                        struct anv_cmd_pipeline_state *pipe_state,
                                        const VkShaderStageFlags dirty,
-                                       const struct anv_shader_bin **shaders,
+                                       const struct anv_shader **shaders,
                                        uint32_t num_shaders);
 
 void genX(cmd_buffer_flush_gfx_hw_state)(struct anv_cmd_buffer *cmd_buffer);

src/intel/vulkan/anv_pipeline.c
@@ -173,16 +173,28 @@ anv_pipeline_finish(struct anv_pipeline *pipeline,
    vk_object_base_finish(&pipeline->vk.base);
 }
 
+VKAPI_ATTR void VKAPI_CALL
+vk_common_DestroyPipeline(VkDevice _device,
+                          VkPipeline _pipeline,
+                          const VkAllocationCallbacks *pAllocator);
+
 void anv_DestroyPipeline(
     VkDevice                                    _device,
     VkPipeline                                  _pipeline,
     const VkAllocationCallbacks*                pAllocator)
 {
    ANV_FROM_HANDLE(anv_device, device, _device);
-   ANV_FROM_HANDLE(anv_pipeline, pipeline, _pipeline);
+   VK_FROM_HANDLE(vk_pipeline, vk_pipeline, _pipeline);
 
-   if (!pipeline)
+   if (!vk_pipeline)
       return;
+
+   if (vk_pipeline->ops != NULL) {
+      vk_common_DestroyPipeline(_device, _pipeline, pAllocator);
+      return;
+   }
+
+   ANV_FROM_HANDLE(anv_pipeline, pipeline, _pipeline);
 
    ANV_RMV(resource_destroy, device, pipeline);
 
@@ -2851,6 +2863,7 @@ anv_compute_pipeline_create(struct anv_device *device,
    return pipeline->base.batch.status;
 }
 
+#if 0
 VkResult anv_CreateComputePipelines(
     VkDevice                                    _device,
     VkPipelineCache                             pipelineCache,
@@ -2885,6 +2898,7 @@ VkResult anv_CreateComputePipelines(
 
    return result;
 }
+#endif
 
 static uint32_t
 get_vs_input_elements(const struct brw_vs_prog_data *vs_prog_data)
@@ -3343,6 +3357,7 @@ anv_graphics_pipeline_create(struct anv_device *device,
    return pipeline->base.base.batch.status;
 }
 
+#if 0
 VkResult anv_CreateGraphicsPipelines(
     VkDevice                                    _device,
     VkPipelineCache                             pipelineCache,
@@ -3388,6 +3403,7 @@ VkResult anv_CreateGraphicsPipelines(
 
    return result;
 }
+#endif
 
 static bool
 should_remat_cb(nir_instr *instr, void *data)
@@ -4083,6 +4099,7 @@ anv_ray_tracing_pipeline_create(
    return pipeline->base.batch.status;
 }
 
+#if 0
 VkResult
 anv_CreateRayTracingPipelinesKHR(
     VkDevice                                    _device,
@@ -4491,3 +4508,4 @@ anv_GetRayTracingShaderGroupStackSizeKHR(
 
    return brw_bs_prog_data_const(bin->prog_data)->max_stack_size;
 }
+#endif

src/intel/vulkan/genX_cmd_buffer.c
@@ -2121,7 +2121,7 @@ emit_direct_descriptor_binding_table_entry(struct anv_cmd_buffer *cmd_buffer,
 static VkResult
 emit_binding_table(struct anv_cmd_buffer *cmd_buffer,
                    struct anv_cmd_pipeline_state *pipe_state,
-                   const struct anv_shader_bin *shader,
+                   const struct anv_shader *shader,
                    struct anv_state *bt_state)
 {
    uint32_t state_offset;
@@ -2153,7 +2153,7 @@ emit_binding_table(struct anv_cmd_buffer *cmd_buffer,
 
       case ANV_DESCRIPTOR_SET_COLOR_ATTACHMENTS:
          /* Color attachment binding */
-         assert(shader->stage == MESA_SHADER_FRAGMENT);
+         assert(shader->vk.stage == MESA_SHADER_FRAGMENT);
          uint32_t index = binding->index < MAX_RTS ?
             cmd_buffer->state.gfx.color_output_mapping[binding->index] :
             binding->index;
@@ -2268,7 +2268,7 @@ emit_binding_table(struct anv_cmd_buffer *cmd_buffer,
 static VkResult
 emit_samplers(struct anv_cmd_buffer *cmd_buffer,
               struct anv_cmd_pipeline_state *pipe_state,
-              const struct anv_shader_bin *shader,
+              const struct anv_shader *shader,
               struct anv_state *state)
 {
    const struct anv_pipeline_bind_map *map = &shader->bind_map;
@@ -2312,7 +2312,7 @@ uint32_t
 genX(cmd_buffer_flush_descriptor_sets)(struct anv_cmd_buffer *cmd_buffer,
                                        struct anv_cmd_pipeline_state *pipe_state,
                                        const VkShaderStageFlags dirty,
-                                       const struct anv_shader_bin **shaders,
+                                       const struct anv_shader **shaders,
                                        uint32_t num_shaders)
 {
    VkShaderStageFlags flushed = 0;
@@ -2322,7 +2322,7 @@ genX(cmd_buffer_flush_descriptor_sets)(struct anv_cmd_buffer *cmd_buffer,
       if (!shaders[i])
          continue;
 
-      mesa_shader_stage stage = shaders[i]->stage;
+      mesa_shader_stage stage = shaders[i]->vk.stage;
       VkShaderStageFlags vk_stage = mesa_to_vk_shader_stage(stage);
       if ((vk_stage & dirty) == 0)
          continue;
@@ -2361,7 +2361,7 @@ genX(cmd_buffer_flush_descriptor_sets)(struct anv_cmd_buffer *cmd_buffer,
          if (!shaders[i])
             continue;
 
-         mesa_shader_stage stage = shaders[i]->stage;
+         mesa_shader_stage stage = shaders[i]->vk.stage;
 
          result = emit_samplers(cmd_buffer, pipe_state, shaders[i],
                                 &cmd_buffer->state.samplers[stage]);

chaotic-cx/mesa-mirror

commit 7a19bde2efaeed3bf2788e4bde2d590f6eaa043e

anv: Update push descriptor flags for RT state
Fixes: e76ed91d3fb7 ("anv: switch over to runtime pipelines")
Cc: mesa-stable
Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>

src/intel/vulkan/anv_cmd_buffer.c
@@ -1245,6 +1245,19 @@ void anv_CmdPushDescriptorSetWithTemplate2KHR(
 }
 
 void
+anv_update_push_descriptor_flags(struct vk_command_buffer *vk_cmd_buffer,
+                                 struct vk_shader *vk_shaders,
+                                 uint32_t shader_count)
+{
+   struct anv_shader *shaders = (struct anv_shader *)vk_shaders;
+   struct anv_cmd_buffer *cmd_buffer =
+      container_of(vk_cmd_buffer, struct anv_cmd_buffer, vk);
+   struct anv_cmd_ray_tracing_state *rt = &cmd_buffer->state.rt;
+
+   update_push_descriptor_flags(&rt->base, &shaders, shader_count);
+}
+
+void
 anv_cmd_buffer_set_rt_state(struct vk_command_buffer *vk_cmd_buffer,
                             VkDeviceSize scratch_size,
                             uint32_t ray_queries)

src/intel/vulkan/anv_private.h
@@ -6126,6 +6126,10 @@ void
 anv_cmd_buffer_set_rt_state(struct vk_command_buffer *vk_cmd_buffer,
                             VkDeviceSize scratch_size,
                             uint32_t ray_queries);
+void
+anv_update_push_descriptor_flags(struct vk_command_buffer *vk_cmd_buffer,
+                                 struct vk_shader *shader,
+                                 uint32_t shader_count);
 
 void
 anv_cmd_buffer_set_stack_size(struct vk_command_buffer *vk_cmd_buffer,

src/intel/vulkan/anv_shader_compile.c
@@ -2039,4 +2039,5 @@ struct vk_device_shader_ops anv_device_shader_ops = {
    .cmd_set_dynamic_graphics_state = vk_cmd_set_dynamic_graphics_state,
    .cmd_set_rt_state               = anv_cmd_buffer_set_rt_state,
    .cmd_set_stack_size             = anv_cmd_buffer_set_stack_size,
+   .update_push_descriptor_flags = anv_update_push_descriptor_flags,
 };