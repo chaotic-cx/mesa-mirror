bminor/mesa-mesa

commit 98293eb81e24759e2a0c1c8d7ec9f192c59bdb9b

anv: add missing device_memory_report for shaders
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: d39e443ef8 ("anv: add infrastructure for common vk_pipeline")
Acked-by: Michael Cheng <michael.cheng@intel.com>
(cherry picked from commit 567c1b3af4ef8e95a3fd1163c95b4b77e25a5046)

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39003>

src/intel/vulkan/anv_shader.c
@@ -630,6 +630,7 @@ anv_shader_create(struct anv_device *device,
    shader->kernel =
       anv_state_pool_alloc(&device->instruction_state_pool,
                            shader_data->prog_data.base.program_size, 64);
+   ANV_DMR_SP_ALLOC(&device->vk.base, &device->instruction_state_pool, shader->kernel);
    if (shader->kernel.alloc_size == 0) {
       result = vk_error(device, VK_ERROR_OUT_OF_DEVICE_MEMORY);
       goto error_embedded_samplers;
@@ -716,6 +717,7 @@ anv_shader_create(struct anv_device *device,
  error_embedded_samplers:
    for (uint32_t s = 0; s < shader->bind_map.embedded_sampler_count; s++)
       anv_embedded_sampler_unref(device, shader->embedded_samplers[s]);
+   ANV_DMR_SP_FREE(&device->vk.base, &device->instruction_state_pool, shader->kernel);
    anv_state_pool_free(&device->instruction_state_pool, shader->kernel);
  error_shader:
    anv_state_pool_free(&device->instruction_state_pool, shader->kernel);

chaotic-cx/mesa-mirror

commit 5bf1a7784afa787e589e9b8a41f384cb902e232f

anv: missing device_memory_report shader free
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 98293eb81e ("anv: add missing device_memory_report for shaders")
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39209>

src/intel/vulkan/anv_shader.c
@@ -23,6 +23,7 @@ anv_shader_destroy(struct vk_device *vk_device,
    for (uint32_t i = 0; i < shader->bind_map.embedded_sampler_count; i++)
       anv_embedded_sampler_unref(device, shader->embedded_samplers[i]);
 
+   ANV_DMR_SP_FREE(&device->vk.base, &device->instruction_state_pool, shader->kernel);
    anv_state_pool_free(&device->instruction_state_pool, shader->kernel);
    anv_reloc_list_finish(&shader->relocs);
    vk_shader_free(vk_device, pAllocator, vk_shader);